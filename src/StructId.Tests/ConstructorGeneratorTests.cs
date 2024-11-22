453235d17fd0b526ab0d33867e95c0e570f8b951
Daniel Cazzulino <daniel@cazzulino.com>
2024-11-22 19:46:45 -0300

    Add unit test for constructor genreator
    We verify no remaining analysis or compile errors should remain after running.

---
+++ src/StructId.Tests/ConstructorGeneratorTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+
+namespace StructId;
+
+public class ConstructorGeneratorTests
+{
+    [Fact]
+    public async Task GenerateRecordConstructor()
+    {
+        var test = new CSharpSourceGeneratorTest<ConstructorGenerator, DefaultVerifier>
+        {
+            TestBehaviors = TestBehaviors.SkipGeneratedSourcesCheck,
+            TestState =
+            {
+                Sources =
+                {
+                    """
+                    using StructId;
+                    namespace Foo;
+
+                    public readonly partial record struct UserId : IStructId<int>;
+                    """,
+                },
+            },
+        }.WithAnalyzerStructId();
+
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task GenerateRecordStringConstructor()
+    {
+        var test = new CSharpSourceGeneratorTest<ConstructorGenerator, DefaultVerifier>
+        {
+            TestBehaviors = TestBehaviors.SkipGeneratedSourcesCheck,
+            TestState =
+            {
+                Sources =
+                {
+                    """
+                    using StructId;
+                    namespace Foo;
+
+                    public readonly partial record struct UserId : IStructId;
+                    """,
+                },
+            },
+        }.WithAnalyzerStructId();
+
+        await test.RunAsync();
+    }
+}
