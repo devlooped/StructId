0007fcbf26c01a864f07ce6b451eb3f2edd332fd
Daniel Cazzulino <daniel@cazzulino.com>
2024-11-22 18:07:38 -0300

    Document main APIs

--- src/StructId/IStructId.cs
+++ src/StructId/IStructId.cs
@@ -2,7 +2,13 @@
 
 namespace StructId;
 
+/// <summary>
+/// Interface for string-based identifiers.
+/// </summary>
 public partial interface IStructId
 {
+    /// <summary>
+    /// Gets the underlying value of the identifier.
+    /// </summary>
     string Value { get; }
 }
--- src/StructId/IStructId`1.cs
+++ src/StructId/IStructId`1.cs
@@ -2,7 +2,14 @@
 
 namespace StructId;
 
+/// <summary>
+/// Interface for struct-based identifiers.
+/// </summary>
+/// <typeparam name="T">The struct type for the inner <see cref="Value"/> of the identifier.</typeparam>
 public partial interface IStructId<T> where T : struct
 {
+    /// <summary>
+    /// Gets the underlying value of the identifier.
+    /// </summary>
     T Value { get; }
 }
