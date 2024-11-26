c82a293b2db2743a9eee94fab617711691177cb5
Daniel Cazzulino <daniel@cazzulino.com>
2024-11-26 19:54:28 -0300

    Add ISpanParsable<TSelf> implementation

--- src/StructId/Templates/ParsableT.cs
+++ src/StructId/Templates/ParsableT.cs
@@ -4,13 +4,30 @@
 using System.Diagnostics.CodeAnalysis;
 using System;
 
-readonly partial record struct TSelf : IParsable<TSelf>
+readonly partial record struct TSelf : ISpanParsable<TSelf>
 {
+    /// <inheritdoc cref="IParsable{TSelf}"/>
     public static TSelf Parse(string s, IFormatProvider? provider) => new(TId.Parse(s, provider));
 
+    /// <inheritdoc cref="IParsable{TSelf}"/>
     public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out TSelf result)
     {
         if (TId.TryParse(s, provider, out var value))
+        {
+            result = new TSelf(value);
+            return true;
+        }
+        result = default;
+        return false;
+    }
+
+    /// <inheritdoc cref="ISpanParsable{TSelf}"/>
+    public static TSelf Parse(ReadOnlySpan<char> input, global::System.IFormatProvider? provider) => new(TId.Parse(input, provider));
+
+    /// <inheritdoc cref="ISpanParsable{TSelf}"/>
+    public static bool TryParse(ReadOnlySpan<char> input, IFormatProvider? provider, out TSelf result)
+    {
+        if (TId.TryParse(input, provider, out var value))
         {
             result = new TSelf(value);
             return true;
