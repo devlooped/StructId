05e99c4b8a8efe6b50ad6bc3f74d3f77d788197a
Daniel Cazzulino <daniel@cazzulino.com>
2024-11-26 19:54:28 -0300

    Add ISpanParsable<TSelf> implementation

--- src/StructId/Templates/TSelf.cs
+++ src/StructId/Templates/TSelf.cs
@@ -2,10 +2,12 @@
 using System.Diagnostics.CodeAnalysis;
 using StructId;
 
-partial record struct TId : IParsable<TId>, IComparable<TId>
+partial record struct TId : ISpanParsable<TId>, IComparable<TId>
 {
     public static TId Parse(string s, IFormatProvider? provider)
         => s is null ? throw new ArgumentNullException(nameof(s)) : new TId();
+
+    public static TId Parse(ReadOnlySpan<char> s, IFormatProvider? provider) => new();
 
     public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out TId result)
     {
@@ -17,6 +19,9 @@ partial record struct TId : IParsable<TId>, IComparable<TId>
         result = default;
         return false;
     }
+
+    public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, [MaybeNullWhen(false)] out TId result)
+        => TryParse(s.ToString(), provider, out result);
 
     public int CompareTo(TId other) => other.CompareTo(this);
 }
