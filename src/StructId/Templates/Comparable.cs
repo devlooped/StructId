c247fa0c44252ed8ced96ab304ac6c9c9d833ed6
Daniel Cazzulino <daniel@cazzulino.com>
2024-11-27 18:07:00 -0300

    Implement comparison operators following IComparable impl.

--- src/StructId/Templates/Comparable.cs
+++ src/StructId/Templates/Comparable.cs
@@ -6,4 +6,16 @@ readonly partial record struct TSelf : IComparable<TSelf>
 {
     /// <inheritdoc/>
     public int CompareTo(TSelf other) => Value.CompareTo(other.Value);
+
+    /// <inheritdoc/>
+    public static bool operator <(TSelf left, TSelf right) => left.Value.CompareTo(right.Value) < 0;
+
+    /// <inheritdoc/>
+    public static bool operator <=(TSelf left, TSelf right) => left.Value.CompareTo(right.Value) <= 0;
+
+    /// <inheritdoc/>
+    public static bool operator >(TSelf left, TSelf right) => left.Value.CompareTo(right.Value) > 0;
+
+    /// <inheritdoc/>
+    public static bool operator >=(TSelf left, TSelf right) => left.Value.CompareTo(right.Value) >= 0;
 }
