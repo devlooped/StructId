8050bb1dac3b0c3efec79a302ab74957e5ee55e3
Daniel Cazzulino <daniel@cazzulino.com>
2024-12-07 17:30:18 -0300

    Fix improper type names in sample templates
    TSelf/TId are not required (pending an analyzer to enforce this).

--- src/StructId.FunctionalTests/ObjectTemplate.cs
+++ src/StructId.FunctionalTests/ObjectTemplate.cs
@@ -2,7 +2,7 @@
 using StructId.Functional;
 
 [TStructId]
-file partial record struct ObjectTemplate(object Value)
+file partial record struct TSelf(object Value)
 {
     // applies to any struct id, whether struct or string
 }
