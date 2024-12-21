d4af15816f7fe6617949df0fa5236ce338812531
Daniel Cazzulino <daniel@cazzulino.com>
2024-12-21 03:52:41 -0300

    Add sample showcasing no namespace is required for ids

---
+++ src/StructId.FunctionalTests/NoNs.cs
@@ -0,0 +1,6 @@
+﻿using StructId.Functional;
+
+// Showcases that types don't need to have a namespace
+public partial record struct NoNsId : IStructId;
+
+public record NoNsProduct(NoNsId Id, string Name);
